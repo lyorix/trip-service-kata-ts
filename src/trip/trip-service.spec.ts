e348cb08b100244c66e850515ec50c6aa0f25c74
svbf8006 <aurelien.bourgeon@orange.com>
2023-03-08 18:22:06 +0100

    add test on the shortest path

--- src/trip/trip-service.spec.ts
+++ src/trip/trip-service.spec.ts
@@ -1,7 +1,17 @@
 import 'jest';
+import { userSession } from '../user/user-session';
+import { TripService } from './trip-service';
+import { User } from '../user/user';
+import { UserNotLoggedInError } from '../error/user-not-logged-in-error';
+
+jest.mock('../user/user-session');
+
+const GUEST: User = null;
 
 describe('TripService', () => {
-    it('should test something', () => {
-        expect(4 + 4).toBe(9);
+    it('should throw exception when user is not logged in', () => {
+        jest.spyOn(userSession, 'getLoggedUser').mockReturnValueOnce(GUEST);
+        const tripService = new TripService();
+        expect(() => tripService.getTripsByUser(undefined)).toThrowError(UserNotLoggedInError);
     });
 });

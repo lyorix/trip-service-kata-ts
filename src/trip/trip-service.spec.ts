c86be9c8e314d2b6f2c4a6a7f4f8d839b835b7a1
svbf8006 <aurelien.bourgeon@orange.com>
2023-03-08 18:23:41 +0100

    add test on the second-shortest path

--- src/trip/trip-service.spec.ts
+++ src/trip/trip-service.spec.ts
@@ -3,15 +3,31 @@ import { userSession } from '../user/user-session';
 import { TripService } from './trip-service';
 import { User } from '../user/user';
 import { UserNotLoggedInError } from '../error/user-not-logged-in-error';
+import { Trip } from './trip';
 
 jest.mock('../user/user-session');
 
 const GUEST: User = null;
+const REGISTERED_USER = new User();
+const ISLAND_TRIP = new Trip();
+let tripService;
 
 describe('TripService', () => {
+    beforeEach(() => {
+        tripService = new TripService();
+        jest.resetAllMocks();
+    });
+
     it('should throw exception when user is not logged in', () => {
         jest.spyOn(userSession, 'getLoggedUser').mockReturnValueOnce(GUEST);
-        const tripService = new TripService();
         expect(() => tripService.getTripsByUser(undefined)).toThrowError(UserNotLoggedInError);
+    });
+
+    it('should not return any trip if users are not friends', () => {
+        jest.spyOn(userSession, 'getLoggedUser').mockReturnValueOnce(REGISTERED_USER);
+        const friend = new User();
+        friend.addTrip(ISLAND_TRIP);
+        const trips = tripService.getTripsByUser(friend);
+        expect(trips.length).toBe(0);
     });
 });
